a3e6b927dd0a0019a13023bd0f1641eb2156186b
csiki <csiki@MARCILAPTOP>
2023-11-14 10:33:16 +0100

    Add project files.

---
+++ API/Controllers/JokesController.cs
@@ -0,0 +1,65 @@
+﻿using API.JokeModels;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    [Route("api/jokes")]
+    [ApiController]
+    public class JokesController : ControllerBase
+    {
+        // GET: api/jokes
+        [HttpGet]
+        public IActionResult Get()
+        {
+            FunnyDatabaseContext context = new FunnyDatabaseContext();
+            return Ok(context.Jokes.ToList());
+        }
+
+        // GET api/jokes/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            FunnyDatabaseContext context = new FunnyDatabaseContext();
+            var keresettVicc = (from x in context.Jokes
+                                where x.JokeSk == id
+                                select x).FirstOrDefault();
+            if (keresettVicc == null)
+            {
+                return NotFound($"Nincs #{id} azonosítóval vicc");
+            }
+            else
+            {
+                return Ok(keresettVicc);
+            }
+        }
+
+        // POST api/<JokesController>
+        [HttpPost]
+        public void Post([FromBody] Joke újVicc)
+        {
+            FunnyDatabaseContext context = new FunnyDatabaseContext();
+            context.Jokes.Add(újVicc);
+            context.SaveChanges();
+        }
+
+        // PUT api/<JokesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<JokesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            FunnyDatabaseContext context = new FunnyDatabaseContext();
+            var törlendőVicc = (from x in context.Jokes
+                                where x.JokeSk == id
+                                select x).FirstOrDefault();
+            context.Remove(törlendőVicc);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ API/JokeModels/Joke.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace API.JokeModels;
+
+public partial class Joke
+{
+    public int JokeSk { get; set; }
+
+    public string? JokeText { get; set; }
+
+    public int UpVotes { get; set; }
+
+    public int DownVotes { get; set; }
+}
