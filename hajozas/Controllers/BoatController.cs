a73f50e0df1af30e0b79921868be20ddd20c5c6a
Marci Csiki-Mara <csikimaramarci@gmail.com>
2023-11-28 12:57:46 +0100

    database schema mapping, created boat controller

---
+++ hajozas/Controllers/BoatController.cs
@@ -0,0 +1,18 @@
+﻿using hajozas.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hajozas.Controllers
+{
+    public class BoatController : Controller
+    {
+        [HttpGet]
+        [Route("questions/all")]
+        public IActionResult AllQuestion()
+        {
+            HajosContext context = new();
+            var kerdesek = from x in context.Questions select x.Question1;
+
+            return Ok(kerdesek);
+        }
+    }
+}
---
+++ hajozas/Models/HajosContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace hajozas.Models;
+
+public partial class HajosContext : DbContext
+{
+    public HajosContext()
+    {
+    }
+
+    public HajosContext(DbContextOptions<HajosContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Question> Questions { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=bit.uni-corvinus.hu;Initial Catalog=hajos;Persist Security Info=True;User ID=hallgato;Password=Password123;Encrypt=false");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Question>(entity =>
+        {
+            entity.ToTable("Question");
+
+            entity.Property(e => e.QuestionId)
+                .ValueGeneratedNever()
+                .HasColumnName("Question_ID");
+            entity.Property(e => e.Question1).HasColumnName("Question");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
