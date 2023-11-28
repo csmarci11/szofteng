20873bb9c315167425b8fddb9793b1f61d3e29cd
Marci Csiki-Mara <csikimaramarci@gmail.com>
2023-11-28 14:13:57 +0100

    Finished javascript, made it an external file. Now you can navigate through questions and the correct answer will be shown. Pictures don't work because they are no longer on the server.

---
+++ hajozas/wwwroot/kerdesek.js
@@ -0,0 +1,62 @@
+﻿var questionId = 1;
+
+window.onload = function (e) {
+    console.log("Oldal betöltve...");
+    document.getElementById("next").onclick = next;
+    document.getElementById("prev").onclick = prev;
+    kerdesBetoltes(questionId)
+}
+
+function kerdesBetoltes(id) {
+    fetch(`/questions/${id}`)
+        .then(response => {
+            if (!response.ok) {
+                console.error(`Hibás válasz: ${response.status}`)
+            }
+            else {
+                return response.json()
+            }
+        })
+        .then(data => kiiratas(data));
+}
+
+function kiiratas(kérdés) {
+    if (!kérdés) return; //Ha undefined a kérdés objektum, nincs mit tenni
+    console.log(kérdés);
+    document.getElementById("kérdés_szöveg").innerText = kérdés.question1
+    document.getElementById("válasz1").innerText = kérdés.answer1
+    document.getElementById("válasz2").innerText = kérdés.answer2
+    document.getElementById("válasz3").innerText = kérdés.answer3
+    jóVálasz = kérdés.correctAnswer;
+    if (kérdés.image) {
+        document.getElementById("kép").src = "https://szoft1.comeback.hu/hajo/" + kérdés.image;
+        document.getElementById("kép").classList.remove("rejtett")
+    }
+    else {
+        document.getElementById("kép").classList.add("rejtett")
+    }
+    //Jó és rossz kérdések jelölésének levétele
+    document.getElementById("válasz1").classList.remove("jó", "rossz");
+    document.getElementById("válasz2").classList.remove("jó", "rossz");
+    document.getElementById("válasz3").classList.remove("jó", "rossz");
+}
+
+function next() {
+    questionId++;
+    kerdesBetoltes(questionId);
+}
+
+function prev() {
+    questionId--;
+    kerdesBetoltes(questionId);
+}
+
+function választás(n) {
+    if (n != jóVálasz) {
+        document.getElementById(`válasz${n}`).classList.add("rossz");
+        document.getElementById(`válasz${jóVálasz}`).classList.add("jó");
+    }
+    else {
+        document.getElementById(`válasz${jóVálasz}`).classList.add("jó");
+    }
+}
